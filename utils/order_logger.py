8899c8f99dcb40dbd924e6c0955173bd23ef791d
shuvotrading35-a11y <shuvotrading35@gmail.com>
2026-06-26 23:41:30 +0600

    Update order_logger.py

--- utils/order_logger.py
+++ utils/order_logger.py
@@ -57,9 +57,9 @@ async def send_order_log(
                 f"👤 𝗨𝘀𝗲𝗿𝗻𝗮𝗺𝗲: {player_id}\n"
                 f"🏷️ 𝗡𝗮𝗺𝗲: {nickname or 'N/A'}\n"
                 f"\n"
-                f"👮🏻‍♂ 𝗕𝗼𝘁: {LOG_BOT_USERNAME}\n"
-                f"📢 𝗢𝗳𝗳𝗶𝗰𝗶𝗮𝗹: {LOG_OFFICIAL_NAME}\n"
-                f"📢 𝗣𝗮𝘆𝗺𝗲𝗻𝘁 𝗣𝗿𝗼𝗼𝗳: {LOG_PAYMENT_NAME}"
+                f"👮🏻‍♂ 𝗕𝗼𝘁: <a href='https://t.me/{LOG_BOT_USERNAME.lstrip('@')}'>𝗖𝗹𝗶𝗰𝗸 𝗛𝗲𝗿𝗲</a>\n"
+                f"📢 𝗢𝗳𝗳𝗶𝗰𝗶𝗮𝗹: <a href='{LOG_OFFICIAL_NAME}'>𝗖𝗹𝗶𝗰𝗸 𝗛𝗲𝗿𝗲</a>\n"
+                f"📢 𝗣𝗮𝘆𝗺𝗲𝗻𝘁 𝗣𝗿𝗼𝗼𝗳: <a href='{LOG_PAYMENT_NAME}'>𝗖𝗹𝗶𝗰𝗸 𝗛𝗲𝗿𝗲</a>"
             )
         else:
             text = (
@@ -72,9 +72,9 @@ async def send_order_log(
                 f"👤 𝗣𝗹𝗮𝘆𝗲𝗿 𝗜𝗗: {player_id}\n"
                 f"🏷️ 𝗡𝗶𝗰𝗸𝗻𝗮𝗺𝗲: {nickname or 'N/A'}\n"
                 f"\n"
-                f"👮🏻‍♂ 𝗕𝗼𝘁: {LOG_BOT_USERNAME}\n"
-                f"📢 𝗢𝗳𝗳𝗶𝗰𝗶𝗮𝗹: {LOG_OFFICIAL_NAME}\n"
-                f"📢 𝗣𝗮𝘆𝗺𝗲𝗻𝘁 𝗣𝗿𝗼𝗼𝗳: {LOG_PAYMENT_NAME}"
+                f"👮🏻‍♂ 𝗕𝗼𝘁: <a href='https://t.me/{LOG_BOT_USERNAME.lstrip('@')}'>𝗖𝗹𝗶𝗰𝗸 𝗛𝗲𝗿𝗲</a>\n"
+                f"📢 𝗢𝗳𝗳𝗶𝗰𝗶𝗮𝗹: <a href='{LOG_OFFICIAL_NAME}'>𝗖𝗹𝗶𝗰𝗸 𝗛𝗲𝗿𝗲</a>\n"
+                f"📢 𝗣𝗮𝘆𝗺𝗲𝗻𝘁 𝗣𝗿𝗼𝗼𝗳: <a href='{LOG_PAYMENT_NAME}'>𝗖𝗹𝗶𝗰𝗸 𝗛𝗲𝗿𝗲</a>"
             )
     else:
         text = (
@@ -86,9 +86,9 @@ async def send_order_log(
             f"👍 𝗔𝗺𝗼𝘂𝗻𝘁: {quantity:,} {service_name}\n"
             f"🔗 𝗣𝗼𝘀𝘁 𝗟𝗶𝗻𝗸:\n{link}\n"
             f"\n"
-            f"👮🏻‍♂ 𝗕𝗼𝘁: {LOG_BOT_USERNAME}\n"
-            f"📢 𝗢𝗳𝗳𝗶𝗰𝗶𝗮𝗹: {LOG_OFFICIAL_NAME}\n"
-            f"📢 𝗣𝗮𝘆𝗺𝗲𝗻𝘁 𝗣𝗿𝗼𝗼𝗳: {LOG_PAYMENT_NAME}"
+            f"👮🏻‍♂ 𝗕𝗼𝘁: <a href='https://t.me/{LOG_BOT_USERNAME.lstrip('@')}'>𝗖𝗹𝗶𝗰𝗸 𝗛𝗲𝗿𝗲</a>\n"
+            f"📢 𝗢𝗳𝗳𝗶𝗰𝗶𝗮𝗹: <a href='{LOG_OFFICIAL_NAME}'>𝗖𝗹𝗶𝗰𝗸 𝗛𝗲𝗿𝗲</a>\n"
+            f"📢 𝗣𝗮𝘆𝗺𝗲𝗻𝘁 𝗣𝗿𝗼𝗼𝗳: <a href='{LOG_PAYMENT_NAME}'>𝗖𝗹𝗶𝗰𝗸 𝗛𝗲𝗿𝗲</a>"
         )
 
     url     = _TG_URL.format(token=LOG_BOT_TOKEN)
